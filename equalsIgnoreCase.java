103dc7d9ea6bbb9f45edc22ae6c38c38da84d811
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-05-21 17:03:02 +0300

    equalsIgnoreCase

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+  public class kisa{
+    public static void main(String[]args){
+      Scanner a = new Scanner(System.in);
+       String x = a.next();
+         System.out.println(x == "a");
+         System.out.println(x.equalsIgnoreCase("A"));
+}}
